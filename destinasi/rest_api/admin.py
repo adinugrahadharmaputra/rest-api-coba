49b3cce8db547b964a5a52f610c00f7255451245
adinugrahadharmaputra <adinugrahadharmaputra@gmail.com>
2021-04-17 15:44:29 +0700

    third commit

--- destinasi/rest_api/admin.py
+++ destinasi/rest_api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from destinasi.models import Destinasi
 # Register your models here.
+
+class DestinasiAdmin(admin, ModelAdmin):
+    list_display = ['name', 'description', 'image_url','address','site', 'created','updated']
+    search_fields = ['name','description', 'address']
+    list_per_page = 5
+
+admin.site.register(Destinasi,DestinasiAdmin)
--- destinasi/rest_api/models.py
+++ destinasi/rest_api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Destinasi(models.Model):
+    name = models.CharField(max_lenght=50)
+    description = models.TextField()
+    image_url = models.TextField()
+    address = models.TextField()
+    site = models.URLField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__ (self):
+        return self.name
